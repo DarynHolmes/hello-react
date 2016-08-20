a2cd93ace26fecef6077f4a583e5a73c873975be
Daryn Holmes <darynholmes@gmail.com>
2016-08-20 17:35:41 +0100

    first simple prac

--- public/app.js
+++ public/app.js
@@ -2,7 +2,8 @@ const Greeter = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Heyo React App</h1>,
+        <h1>Heyo React App</h1>
+        <p>This is from the component</p>
       </div>
     );
   }

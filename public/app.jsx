604bf641e83e500a84bbad81b1e144cf479ba6fb
Daryn Holmes <darynholmes@gmail.com>
2016-08-27 10:51:37 +0100

    breaking the app up into modules

--- public/app.jsx
+++ public/app.jsx
@@ -1,29 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-
-const GreeterMessage = require("./components/GreeterMessage");
-const GreeterForm = require("./components/GreeterForm");
-
-const Greeter = React.createClass({
-
-    getInitialState: function() {
-        return {name: this.props.name, message: this.props.message};
-    },
-    handleNewParams: function(params) {
-        this.setState(params);
-    },
-    render: function() {
-        const name = this.state.name;
-        const message = this.state.message;
-
-        return (
-            <div>
-                <GreeterMessage name={name} message={message} />
-                <GreeterForm onNewParams={this.handleNewParams}/>
-            </div>
-        );
-    }
-});
+const Greeter = require("./components/Greeter");
 
 var name = "Bobby";
 
--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,6 +1,26 @@
+const React = require('react');
+const GreeterMessage = require("./GreeterMessage");
+const GreeterForm = require("./GreeterForm");
 
-function greeter() {
-  document.write("Hi from greeter!!!");
-}
+const Greeter = React.createClass({
 
-module.exports = greeter;
+    getInitialState: function() {
+        return {name: this.props.name, message: this.props.message};
+    },
+    handleNewParams: function(params) {
+        this.setState(params);
+    },
+    render: function() {
+        const name = this.state.name;
+        const message = this.state.message;
+
+        return (
+            <div>
+                <GreeterMessage name={name} message={message} />
+                <GreeterForm onNewParams={this.handleNewParams}/>
+            </div>
+        );
+    }
+});
+
+module.exports = Greeter;
